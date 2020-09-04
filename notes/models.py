98ddda3352c7d3fa3ed4e16de232b2de16574993
Milan Shah <milanshah1@gmail.com>
2020-09-04 13:02:58 +0530

    first commit

---
+++ notes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Note(models.Model):
+
+    title = models.CharField(max_length=200)
+    desc = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    completed = models.BooleanField(default=False)
