e6ced0341342dad0ec7d5ccbf7b4ef3b59c1fc25
Milan Shah <milanshah1@gmail.com>
2020-09-05 12:37:14 +0530

    Update to use dynamic urls

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import show_all_notes
+from .views import show_all_notes, show_single_note
 
 urlpatterns = [
     path('', show_all_notes, name='show_all_notes'),
+    path('<int:note_id>', show_single_note, name='show_single_note')
     # path('add', add_note, name='add_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -6,3 +6,7 @@ from .models import Note
 def show_all_notes(request):
     all_notes = Note.objects.all()
     return render(request, 'notes/all_notes.html', { 'notes': all_notes })
+
+def show_single_note(request, note_id):
+    single_note = Note.objects.get(pk=note_id)
+    return render(request, 'notes/single_note.html', { 'note': single_note })
